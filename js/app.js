43f767a14e9011d15de03a5d68c4dc9850554cd7
David Brownman <beamneocube@gmail.com>
2015-02-12 18:03:07 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('quidTimer', ['timer'])
+
+  .controller('MainCtrl', ['$scope', function($scope){
+    $scope.message = 'hiiiii';
+    $scope.init = true;
+    $scope.timerRunning = false;
+
+    $scope.startTimer = function() {
+      if ($scope.init) {
+        $scope.$broadcast('timer-start');
+        $scope.init = false;
+      }
+      else {
+        $scope.$broadcast('timer-resume');
+      }
+      $scope.timerRunning = true;  
+    };
+
+    $scope.stopTimer = function() {
+      $scope.$broadcast('timer-stop');
+      $scope.timerRunning = false;
+    };
+  }]);
