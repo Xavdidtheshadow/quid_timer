5361a7409e551820aa4a35650394e0c2d9c6d6e1
David Brownman <beamneocube@gmail.com>
2015-02-13 01:34:27 -0800

    trying a different implementation. faster, but not fast enough

--- js/app.js
+++ js/app.js
@@ -5,19 +5,19 @@ var app = angular.module('quidTimer', ['timer'])
     $scope.init = true;
     $scope.timerRunning = false;
 
-    $scope.startTimer = function() {
-      if ($scope.init) {
-        $scope.$broadcast('timer-start');
-        $scope.init = false;
-      }
-      else {
-        $scope.$broadcast('timer-resume');
-      }
-      $scope.timerRunning = true;  
-    };
+    // $scope.startTimer = function() {
+    //   if ($scope.init) {
+    //     $scope.$broadcast('timer-start');
+    //     $scope.init = false;
+    //   }
+    //   else {
+    //     $scope.$broadcast('timer-resume');
+    //   }
+    //   $scope.timerRunning = true;  
+    // };
 
-    $scope.stopTimer = function() {
-      $scope.$broadcast('timer-stop');
-      $scope.timerRunning = false;
-    };
+    // $scope.stopTimer = function() {
+    //   $scope.$broadcast('timer-stop');
+    //   $scope.timerRunning = false;
+    // };
   }]);
